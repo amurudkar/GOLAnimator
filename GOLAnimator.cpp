cf308cda4d8928d4596b2671f9ca554f08f8884a
Adwait <adwait_m@rocketmail.com>
2019-02-23 21:02:30 -0500

    Add project files.

---
+++ GOLAnimator.cpp
@@ -0,0 +1,12 @@
+﻿// GOLAnimator.cpp : Defines the entry point for the application.
+//
+
+#include "GOLAnimator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
